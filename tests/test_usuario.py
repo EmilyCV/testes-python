746be2a9b9d16e967394fed1d806c7e1e7fcc231
EmilyCV <emily_cvasconcelos@hotmail.com>
2022-05-24 09:50:30 -0300

    first tests with pytest

---
+++ tests/test_usuario.py
@@ -0,0 +1,17 @@
+import pytest
+from src.leilao.dominio import Leilao, Usuario
+
+
+@pytest.fixture
+def vini():
+    return Usuario('Vini', 100.0)
+
+
+@pytest.fixture
+def leilao():
+    return Leilao('Celular')
+
+
+def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance(vini, leilao):
+    vini.propoe_lance(leilao, 50.0)
+    assert vini.carteira == 50.0
